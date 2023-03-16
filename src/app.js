e6c6f9dde1d020e141a444dab1e60ea8d84fbdc8
khaledxyz <attano001@outlook.com>
2023-03-16 23:59:02 +0100

    initialize app

---
+++ src/app.js
@@ -0,0 +1,33 @@
+// * Imports
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+
+const errorHandler = require('./middleware/errorHandler');
+const connectDB = require('./config/connectDB.js');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// * Middleware * //
+app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(bodyParser.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: false, limit: "1mb" }));
+app.use(errorHandler);
+
+// * Routes * //
+app.use('/api/auth', require('./routes/authRouter'));
+app.use('/api/users', require('./routes/usersRouter'));
+app.use('/api/posts', require('./routes/postsRouter'));
+
+// * Connection * //
+app.listen(PORT, () => {
+    console.log('---------------------');
+    console.log(
+        'Dev server running at  >'.green,
+        `http://localhost:${PORT}`.blue
+    );
+    connectDB();
+});
---
+++ src/config/connectDB.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(
+            'Connected to MongoDB   >'.green,
+            connection.connection.host.blue
+        )
+    }
+    catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
