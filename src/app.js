6f0737ddc143b2ad73921a0cd2383ce51ed931e7
khaledxyz <attano001@outlook.com>
2023-03-18 09:33:29 +0100

    feat: add logger

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
+const morgan = require('morgan')
 
 const errorHandler = require('./middleware/errorHandler');
-const connectDB = require('./config/connectDB.js');
-
+const connectDB = require('./config/connectDB');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -16,12 +16,14 @@ app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(bodyParser.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: false, limit: "1mb" }));
 app.use(errorHandler);
+app.use(morgan('tiny'));
 
 // * Routes * //
 app.use('/api/auth', require('./routes/authRouter'));
 app.use('/api/users', require('./routes/usersRouter'));
 app.use('/api/posts', require('./routes/postsRouter'));
 app.use('/api/events', require('./routes/eventsRouter'));
+// app.use('/api/reports', require('./routes/reportsRouter'));
 
 // * Connection * //
 app.listen(PORT, () => {
