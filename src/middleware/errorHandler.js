795862113a6c3f644100c493476ec5d89021cba3
khaledxyz <attano001@outlook.com>
2023-03-18 10:07:30 +0100

    fix: capitalization

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -2,7 +2,7 @@ const errorHandler = (Error, req, res, next) => {
     res.status(Error.status || 500);
     res.json({
         message: Error.message || 'Oops, Something went wrong :/',
-        stack: process.env.NODE_ENV === 'development' ? Error.stack : null
+        stack: process.env.NODE_ENV === 'DEVELOPMENT' ? Error.stack : null
     })
     next()
 };
